2bf131349f06b83389e242de041cf2059d8b0d43
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2020-12-03 20:41:21 +0530

    Add files via upload

---
+++ userbot/plugins/love.py
@@ -0,0 +1,38 @@
+#This plugin made by 𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋 owner @H1M4N5HU0P
+
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events	
+@borg.on(admin_cmd(pattern="love", outgoing=True))
+async def _(event):
+		await event.edit("  I ♥ YOU  ")
+		await asyncio.sleep(1)
+		await event.edit("😍")
+		await asyncio.sleep(1)
+		await event.edit("❤️")
+		await asyncio.sleep(1)
+		await event.edit("💋")
+		await asyncio.sleep(1)
+		await event.edit("😘")
+		await asyncio.sleep(1)
+		await event.edit("😉")
+		await asyncio.sleep(1)
+		await event.edit("  I LOVE YOU BABY😍😘😘  ")
+		await asyncio.sleep(1)
+		await event.edit("😍")
+		await asyncio.sleep(1)
+		await event.edit("😘")
+		await asyncio.sleep(1)
+		await event.edit("🥰")
+		await asyncio.sleep(1)
+		await event.edit("❤️")
+		await asyncio.sleep(1)
+		await event.edit("💋")
+		await asyncio.sleep(1)
+		await event.edit("  I WANT YOU♥😍😘😘  ")
+		await asyncio.sleep(1)
+		await event.edit("❤️")
+		await asyncio.sleep(1)
+		await event.edit("  I LOVE YOU VERY MUCH😍😘😘❤️❤️  ")
+		
---
+++ userbot/plugins/mafia-game.py
@@ -0,0 +1,61 @@
+#Made by @Kraken_the_badass for @HellBot_Official
+
+"""Emoji
+Available Commands:
+click gift as soon as fast as possible
+.hgame
+build by @Hack12R..
+Second game is a xogame
+Command:- .xogame .... By @H1M4N5HU0P"""
+
+from telethon import events
+import asyncio
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from var import Var
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1.2
+    animation_ttl = range(0, 14)
+    input_str = event.pattern_match.group(1)
+    if input_str == "game":
+        await event.edit(input_str)
+        animation_chars = [
+       
+            "**Welcome To 𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋 Repo Game**",
+            "**Click The Gift As Fast As Possible**",
+            "**Game Starts in 3**",
+            "**Game Starts in 2**",
+            "**Game Starts in 1**",    
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆[🎁](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆[🎁](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇[🎁](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆[🎁](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)🎆\n🎇🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n[🎁](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)🎆🎇🎆🎇🎆🎇",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇[🎁](https://H1M4N5HU0P/MAFIA-USERBOT)\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆",
+            "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇\n🎇🎆🎇🎆🎇🎆🎇",
+            "**Game Over**"
+ ]
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 14])
+
+
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from var import Var
+
+@borg.on(admin_cmd(pattern="xogame$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
