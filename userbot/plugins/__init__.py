80868b729725021d679fdd01c56c56afadfe847d
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2020-12-04 11:59:41 +0530

    Add files via upload

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,24 +1,6 @@
-import asyncio
-import os
-import shlex
-from os import getcwd
-from os.path import basename, joinfrom textwrap import wrap
-from typing import Optional, Tuple import numpy as np
-from PIL import Image, ImageDraw, ImageFont
-from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import simpdef
 
 
+simpmusic = simpdef.simpmusic 
+simpmusicvideo = simpdef.simpmusicvideo
 
-
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
