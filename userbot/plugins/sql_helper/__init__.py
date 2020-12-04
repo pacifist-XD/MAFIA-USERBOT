c5cf7cb6fbf62e4e71d417d896227cd8386ab955
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2020-12-04 17:27:58 +0530

    Add files via upload

--- userbot/plugins/sql_helper/__init__.py
+++ userbot/plugins/sql_helper/__init__.py
@@ -1,15 +1,15 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
-from var import Var
+from userbot import Config
 
 
 def start() -> scoped_session:
-    engine = create_engine(Var.DB_URI)
+    engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -20,5 +20,7 @@ try:
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    print("DB_URI is not configured. Features depending on the database might have issues.")
+    print(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
     print(str(e))
