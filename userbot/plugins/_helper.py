99ab3ffe4c72f8b0a8124072756d90fa47530f39
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2020-12-09 10:16:00 +0530

    from userbot import CMD_LIST, CMD_HELP from userbot import ALIVE_NAME from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd from platform import uname import sys from telethon import events, functions, __version__  DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia Userbot"   @bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True)) async def cmd_list(event):     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!", "-", "_"):         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER         input_str = event.pattern_match.group(1)         if tgbotusername is None or input_str == "text":             string = ""             for i in CMD_LIST:                 string += "🤩️" + i + "\n"                 for iter_list in CMD_LIST[i]:                     string += "    `" + str(iter_list) + "`"                     string += "\n"                 string += "\n"             if len(string) > 4095:                 with io.BytesIO(str.encode(string)) as out_file:                     out_file.name = "cmd.txt"                     await bot.send_file(                         event.chat_id,                         out_file,                         force_document=True,                         allow_cache=False,                         caption="**COMMANDS** In Mafia Userbot",                         reply_to=reply_to_id                     )                     await event.delete()             else:                 await event.edit(string)         elif input_str:             if input_str in CMD_LIST:                 string = "Commands found in {}:".format(input_str)                 for i in CMD_LIST[input_str]:                     string += "    " + i                     string += "\n"                 await event.edit(string)             else:                 await event.edit(input_str + " is not a valid plugin!")         else:             help_string = f"""Mafia Userbot Helper Provided by 🔥{DEFAULTUSER}🔥 \n `Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""             results = await bot.inline_query(  # pylint:disable=E0602                 tgbotusername,                 help_string             )             await results[0].click(                 event.chat_id,                 reply_to=event.reply_to_msg_id,                 hide_via=True             )             await event.delete()  @borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602 async def _(event):     if event.fwd_from:         return     result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602     await event.edit(result.stringify())   @borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602 async def _(event):     if event.fwd_from:         return     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602     result = result.stringify()     logger.info(result)  # pylint:disable=E0602     await event.edit("""Telethon UserBot Powered by @MAFIA_USERBOT""")   @borg.on(admin_cmd(pattern="syntax (.*)")) async def _(event):     if event.fwd_from:         return     plugin_name = event.pattern_match.group(1)      if plugin_name in CMD_LIST:         help_string = CMD_LIST[plugin_name].__doc__         unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           @MAFIA_USERBOT"          if help_string:             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"         else:             plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."     else:          plugin_syntax = "Enter valid **Plugin** name.\nDo `.plinfo` or `.help` to get list of valid plugin names."      await event.edit(plugin_syntax)

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -29,7 +29,7 @@ async def cmd_list(event):
                         out_file,
                         force_document=True,
                         allow_cache=False,
-                        caption="**COMMANDS** In HellBot",
+                        caption="**COMMANDS** In Mafia Userbot",
                         reply_to=reply_to_id
                     )
                     await event.delete()
