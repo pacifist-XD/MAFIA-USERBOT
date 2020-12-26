7b20e576943442cd63f072fe23007fd646451655
HellBoy-OP <ghosthellboy4@gmail.com>
2020-12-26 11:54:01 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!"
+            reply_pop_up_alert = (
+                "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -53,7 +55,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!!"
+            reply_pop_up_alert = (
+                "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
