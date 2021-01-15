7517d23da7a9c849366cb368ce93d982ac00ab60
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-01-15 14:24:22 +0530

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,18 +1,32 @@
-from userbot import ALIVE_NAME, mafiaversion
-from userbot.utils import admin_cmd, sudo_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋"
+# Thanks to Sipak bro and Aryan.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking) && @Hell boy_pikachu
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# Porting in Mafia Userbot by @H1M4N5HU0P
 
-ludosudo = Config.SUDO_USERS
+import asyncio
+import random
+from telethon import events
+from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME
+from telethon.tl.types import ChannelParticipantsAdmins
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ÂÝŮ$HópBØȚ"
 
-if ludosudo:
-    sudou = "True"
-else:
-    sudou = "False"
+# Thanks to Sipak bro and Raganork.. 
+# animation Idea by @NOOB_GUY_OP (Sipakisking)
+# Made by @ROMANTIC_KILLER...and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for ÂÝŮ$HópBØȚ
 
-mafia = bot.uid
-
-PM_IMG = "https://telegra.ph/file/502d3b7372c1c395bba5a.mp4"
+edit_time = 5
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/87bf8d6c308ee281068f2.png"
+file2 = "https://telegra.ph/file/88b9948f4a5c90f106d18.png"
+file3 = "https://telegra.ph/file/efa21255e77f4ba3b6aa2.png"
+file4 = "https://telegra.ph/file/56bd966cd3d76d977a3f7.mp4"
+""" =======================CONSTANTS====================== """
 pm_caption = "__**🔥🔥𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋 𝕀𝕊 𝔸𝕃𝕀𝕍𝔼🔥🔥**__\n\n"
 
 pm_caption += (
@@ -32,12 +46,37 @@ pm_caption += "😎CREATOR😎    : [Himanshu](https://t.me/H1M4N5HU0P)\n\n"
 pm_caption += "🤩SUPPORTER🤩    :[HellBoy](https://t.me/kraken_the_badass)\n\n"
 
 pm_caption += "      [✨REPO✨](https://github.com/H1M4N5HU0P/MAFIA-USERBOT) 🔹 [📜License📜](https://github.com/H1M4N5HU0P/MAFIA-USERBOT/blob/main/LICENSE)"
-# @command(outgoing=True, pattern="^.alive$")
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-async def amireallyalive(alive):
-    await alive.get_chat()
+
+@borg.on(admin_cmd(pattern=r"alive"))
+
+async def amireallyalive(yes):
+    chat = await yes.get_chat()
+
+    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
+
     await alive.delete()
+    
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
     await alive.delete()
