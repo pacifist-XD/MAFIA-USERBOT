729cd03f29a1436a637d15e43d08a5789bfb8253
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-01-24 00:15:14 +0530

    Update hellbot.py

--- userbot/plugins/hellbot.py
+++ userbot/plugins/hellbot.py
@@ -12,7 +12,7 @@ from uniborg.util import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="sorry ?(.*)"))
+@borg.on(admin_cmd(pattern="hellbot ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("Click [here](https://github.com/HellBoy-OP/HellBot) to open this 🔥**Lit AF!!**🔥 **Hêllẞø†** Repo.. Join channel :- @HellBot_Official Repo Uploaded By [𝕄𝔸𝔽𝕀𝔸 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://github.com/H1M4N5HU0P/MAFIA-USERBOT)")
