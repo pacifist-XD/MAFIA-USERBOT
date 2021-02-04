a671fb5e406485af7192545284fe9fa756fea47f
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-02-04 18:36:14 +0530

    Add files via upload

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,9 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = (
-                "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!"
-            )
+            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -55,9 +53,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = (
-                "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!!"
-            )
+            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @MAFIA_USERBOT!!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
