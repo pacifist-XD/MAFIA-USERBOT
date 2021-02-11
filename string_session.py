7f9ad4719acab2463afc21376daefe1ed41152c8
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-02-12 00:25:23 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,35 +1,23 @@
-from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+print ("")
+print ("")
+print("""processing.......""")
 
-print("")
-print("""Welcome To MAFIA USERBOT String Session\nGenerator By @H1M4N5HU0P\n\n""")
-print("""Special Thanks To My Buddy Respective HellBot\nOwner @Kraken_The_BadASS\n\n""")
-print("""Enter Your Valid Details To Continue!\n\n """)
-
-API_KEY = input("API_ID:  ")
-API_HASH = input("API_HASH:  ")
-
+API_KEY = '1754367'
+API_HASH = "231b8cc6cca12ee51a85cf543321f476"
 while True:
-    try:
-        with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-            print(
-                "String Session Sucessfully Sent To Your Saved Message, Store It To A Safe Place!!\n\n "
-            )
-            print("")
-            session = client.session.save()
-            client.send_message(
-                "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @MAFIA_USERBOT For Any Help!\n\n",
-            )
-
-            print(
-                "Thanks for Choosing Mafia Userbot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
-            )
-    except:
-        print("")
-        print(
-            "Wrong phone number \n make sure its with correct country code. Example : +918925534834! Kindly Retry"
-        )
-        print("")
-        continue
-    break
+  try:
+   with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+      print("")
+      session = client.session.save()
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(tap to copy)👇 \n\n `{session}`")
+      print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
+      print("Store it safe !!")
+  except:
+   print ("")
+   print ("Wrong phone number \n make sure its with correct  country code")
+   print ("")
+   continue
+  break
+    
